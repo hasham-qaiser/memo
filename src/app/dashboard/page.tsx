a01a7ff491fb8b39f12df24f8f42b8b3054d324c
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2023-10-15 21:10:40 -0700

    start dash

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+const DashboardPage = (props: Props) => {
+  return (
+    <>
+      <div className="grainy min-h-screen">
+        <div className="max-w-7xl mx-auto p-10">
+          <div className="h-14"></div>
+          <div className="flex justify-between items-center md:flex-row flex-col">
+            <div className="flex items-center">
+              <Link href="/">
+                <Button className="bg-green-600">
+                  <ArrowLeft className="mr-1 w-4 h-4" />
+                  Back
+                </Button>
+              </Link>
+              <div className="w-4"></div>
+              <h1 className="text-3xl font-bold text-gray-900">My Notes</h1>
+              <div className="w-4"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DashboardPage;
