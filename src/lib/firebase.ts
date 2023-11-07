c403ede00427cf5ac04e4ea81fc0179134dfd3f3
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2023-11-07 11:24:30 -0800

    note function works, saves and deletes, openai dalle api works, firebase connected

---
+++ src/lib/firebase.ts
@@ -0,0 +1,36 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "potion-c4f03.firebaseapp.com",
+  projectId: "potion-c4f03",
+  storageBucket: "potion-c4f03.appspot.com",
+  messagingSenderId: "563541819917",
+  appId: "1:563541819917:web:b67e0bb7c92611b5914b44",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const storage = getStorage(app);
+
+export async function uploadFileToFirebase(image_url: string, name: string) {
+  try {
+    const response = await fetch(image_url);
+    const buffer = await response.arrayBuffer();
+    const file_name = name.replace(" ", "") + Date.now + ".jpeg";
+    const storageRef = ref(storage, file_name);
+    await uploadBytes(storageRef, buffer, {
+      contentType: "image/jpeg",
+    });
+    const firebase_url = await getDownloadURL(storageRef);
+    return firebase_url;
+  } catch (error) {
+    console.error(error);
+  }
+}
