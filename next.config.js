d5e935100138651b1413b5fdab2ff2ff2f156089
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2023-12-13 11:40:12 -0800

    fixed next.config error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["firebasestorage.googleapis.com"],
+    domains: [
+      "oaidalleapiprodscus.blob.core.windows.net",
+      "firebasestorage.googleapis.com",
+    ],
   },
   typescript: {
     ignoreBuildErrors: true,
