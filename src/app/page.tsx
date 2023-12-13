ecc98356a9c199e2557e617a070769886787daf2
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2023-12-13 11:27:38 -0800

    update overhaul begin change text color in landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import TypewriterTitle from "@/components/TypewriterTitle";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-r min-h-screen grainy from-rose-100 to-blue-100">
+    <div className="bg-gradient-to-r min-h-screen grainy from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <h1 className="font-semibold text-7xl text-center">
-          AI <span className="text-green-600 font-bold">note taking</span>{" "}
+          <span className="text-blue-500 font-bold">Potion.</span>{" "}
+        </h1>
+        <br></br>
+        <br></br>
+        <h1 className="font-semibold text-7xl text-center">
+          Your AI <span className="text-blue-500 font-bold">note taking</span>{" "}
           assistant.
         </h1>
         <div className="mt-4"></div>
@@ -19,7 +23,7 @@ export default function Home() {
         <div className="mt-8"></div>
         <div className="flex justify-center">
           <Link href="/dashboard">
-            <Button className="bg-green-600">
+            <Button className="bg-blue-500">
               Get Started
               <ArrowRight className="ml-2 w-5 h-5" strokeWidth={3} />
             </Button>
