5e78e997895b30ca1641d3b3b21a8d42701f62b1
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2023-12-21 21:27:07 -0800

    tried to fix heading error, still broken

--- src/app/notebook/[noteId]/page.tsx
+++ src/app/notebook/[noteId]/page.tsx
@@ -36,15 +36,17 @@ const NotebookPage = async ({ params: { noteId } }: Props) => {
     <div className="min-h-screen grainy p-8">
       <div className="max-w-4xl mx-auto">
         <div className="border shadow-xl border-stone-200 rounded-lg p-4 flex items-center">
-          <Link href={"/dashboard"}>
-            <Button className="bg-blue-500 size='sm">Back</Button>
+          <Link href="/dashboard">
+            <Button className="bg-blue-500" size="sm">
+              Back
+            </Button>
           </Link>
           <div className="w-3"></div>
           <span className="font-semibold">
             {user.firstName} {user.lastName}
           </span>
           <span className="inline-block mx-1">/</span>
-          <span className="text-stone-500 font-semibold ">{note.name}</span>
+          <span className="text-stone-500 font-semibold">{note.name}</span>
           <div className="ml-auto">
             <DeleteButton noteId={note.id} />
           </div>
